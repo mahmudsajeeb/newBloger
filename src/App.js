38d5be95fe039bd5c0c48ad4020b834f8ef0f711
mahmudsajib <mahmudsajib082@gmail.com>
2022-04-15 02:12:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react'
+import  Axios  from 'axios'
+ 
+import { Button,Card , Container,Row} from 'react-bootstrap';
 
-function App() {
+export default function App() {
+  const [data,setData] = useState([])
+ 
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=d6e59e6b8e0f4cb2b84570efa5cc5ecb")
+    .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+    .then(data => setData(data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container my-5'> 
+        <Container>
+            <Row>
+              
+              {
+          data.map((value) =>{
+            
+            return(
+              
+                <div className="col-4">
+                <Card style={{ width: '24rem' }}>
+                 
+                    <Card.Img variant="top" src={value.urlToImage} />
+                    <Card.Body>
+                      <Card.Title>{value.title}</Card.Title>
+                      <span> <strong>Author</strong>: {value.author}</span>
+                      <Card.Text>
+                        {value.description}
+                      </Card.Text>
+                    <Button variant="primary">Read More</Button>
+                  </Card.Body>
+                </Card>
+                </div>
+             
+            ) 
+          })
+        }
+          
+            </Row>
+        </Container>
     </div>
-  );
+  )
 }
-
-export default App;
